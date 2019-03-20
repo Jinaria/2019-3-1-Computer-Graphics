53200a0376eec31282d24faeca1c515db5baf184
2014005187 <2014005187@hanyang.ac.kr>
2019-03-20 13:50:05 +0900

    LAB3

--- 2014005187-3-1.py
+++ 2014005187-3-1.py
@@ -1,9 +1,10 @@
+import numpy as np
 import glfw
 from OpenGL.GL import *
 
 def render(T):
-	glClear(GL_COLOR_BUFFER_BIT)
-	glLoadIdentity()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
     # draw coordinate
     glBegin(GL_LINES)
     glColor3ub(255, 0, 0)
@@ -20,3 +21,28 @@ def render(T):
     glVertex2fv((T @ np.array([.0, .0, 1.]))[:-1])
     glVertex2fv((T @ np.array([.5, .0, 1.]))[:-1])
     glEnd()
+
+def main():
+    if not glfw.init():
+        return
+    window = glfw.create_window(480, 480, "2014005187-3-1", None, None)
+    if not window:
+        glfw.terminate()
+        return
+    glfw.make_context_current(window)
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+        T = np.array([[1., 0., .5],
+                     [0., 1., .0],
+                     [0., 0., 1.]])
+
+        t = glfw.get_time()
+        R = np.array([[np.cos(t), -np.sin(t), 0.],
+                      [np.sin(t), np.cos(t),  0.], 
+                      [0.,         0.,          1.]])
+        render(R @ T)
+        glfw.swap_buffers(window)
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
